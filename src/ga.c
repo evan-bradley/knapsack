b70cab23ea0bbff1805165f84d761b78c3cdb86e
Evan Bradley <evanbradley@airmail.cc>
2018-04-10 13:31:07 -0400

    GA should now work; changed bit array to normal array for now.

--- src/ga.c
+++ src/ga.c
@@ -1,41 +1,61 @@
 #include "kp.h"
+#include <memory.h>
 
 #define get_bit(arr, i) (arr[i / 32] & (1 << (i % 32)))
 #define set_bit(arr, i) (arr[i / 32] |= (1 << (i % 32)))
 #define unset_bit(arr, i) (arr[i / 32] &= ~(1 << (i % 32)))
 
-void gen_population_nums(uint32_t **pop, uint32_t len, uint32_t genes)
+void gen_population_nums(uint32_t *pop, uint32_t len, uint32_t genes)
 {
   for (uint32_t i = 0; i < len; i++) {
     for (uint32_t j = 0; j < genes; j++) {
-      pop[i][j] = rand() % (genes - 1);
+      pop[i * genes + j] = rand() % 2;
     }
   }
 }
 
-void gen_population_bits(uint32_t **pop, uint32_t len, uint32_t genes, uint32_t bits)
+void print_cell(uint32_t *cell, uint32_t len)
+{
+    for (uint32_t i = 0; i < len; i++) {
+        printf("%u", cell[i]);
+    }
+    printf("\n");
+}
+
+void print_cell_nodes(uint32_t *cell, uint32_t len, knapsack_node_t *knapsack)
+{
+    uint32_t value = 0, weight = 0;
+    for (uint32_t i = 0; i < len; i++) {
+        if (cell[i] == 1) {
+            value += knapsack[i].value;
+            weight += knapsack[i].weight;
+            printf("{ value: %u\tweight: %u }\n", knapsack[i].value, knapsack[i].weight);
+        }
+    }
+    printf("value: %u\t weight: %u\n", value, weight);
+}
+
+void print_population(uint32_t **pop, uint32_t len, uint32_t genes)
 {
   for (uint32_t i = 0; i < len; i++) {
-    for (uint32_t j = 0; j < bits; j++) {
-      set_bit(pop[i], rand() % (genes - 1));
-    }
+      print_cell(pop[i], genes);
   }
 }
 
 uint64_t test_fitness(uint32_t *cell, uint32_t len, knapsack_node_t *knapsack, uint64_t max_weight)
 {
-  uint64_t value_total = 0;
-  uint64_t weight_total = 0;
+  uint32_t value_total = 0;
+  uint32_t weight_total = 0;
 
   for (uint32_t i = 0; i < len; i++) {
-    if (get_bit(cell, i) == 1) {
+    if (cell[i] == 1) {
       weight_total += knapsack[i].weight;
       value_total += knapsack[i].value;
 
       // Note that breaking once we hit the max weight only counts the values up
       // to that point, and so might induce bias.
       if (weight_total > max_weight) {
-        value_total -= knapsack[i].value;
+        value_total = value_total / 2;
         break;
       }
     }
@@ -49,6 +69,30 @@ uint64_t test_fitness(uint32_t *cell, uint32_t len, knapsack_node_t *knapsack, u
   return value_total + (uint64_t) (value_total / weight_total);
 }
 
+uint32_t get_weight(uint32_t *cell, uint32_t len, knapsack_node_t *knapsack)
+{
+    uint32_t weight = 0;
+    for (uint32_t i = 0; i < len; i++) {
+        if (cell[i] == 1) {
+            weight += knapsack[i].weight;
+        }
+    }
+
+    return weight;
+}
+
+uint32_t get_value(uint32_t *cell, uint32_t len, knapsack_node_t *knapsack)
+{
+    uint32_t value = 0;
+    for (uint32_t i = 0; i < len; i++) {
+        if (cell[i] == 1) {
+            value += knapsack[i].value;
+        }
+    }
+
+    return value;
+}
+
 void breed(uint32_t *child1, uint32_t *child2, uint32_t *parent1, uint32_t *parent2, uint32_t half)
 {
   for (uint32_t i = 0; i < half; i++) {
@@ -57,6 +101,11 @@ void breed(uint32_t *child1, uint32_t *child2, uint32_t *parent1, uint32_t *pare
     child2[i] = parent1[half - i];
     child2[i + half] = parent2[i];
   }
+}
+
+void mutate(uint32_t *cell, uint32_t len)
+{
+    cell[rand() % len] = !cell[rand() % len];
 }
 
 void copy_arr(uint32_t **arr0, uint32_t **arr1, uint32_t m, uint32_t n)
@@ -71,24 +120,18 @@ void copy_arr(uint32_t **arr0, uint32_t **arr1, uint32_t m, uint32_t n)
 void cycle(knapsack_node_t *knapsack, uint32_t pop_size,
            uint32_t item_count, uint32_t max_weight, uint32_t cycles)
 {
-  uint32_t** pop0 = (uint32_t **) malloc(pop_size * sizeof(uint32_t *));
-  for (uint32_t i = 0; i < pop_size; i++) {
-    pop0[i] = (uint32_t *) calloc(item_count / 32, sizeof(uint32_t));
-  }
+  uint32_t* pop0 = (uint32_t *) calloc(pop_size * item_count, sizeof(uint32_t));
+  uint32_t* pop1 = (uint32_t *) calloc(pop_size * item_count, sizeof(uint32_t));
+  uint32_t* best_cell = (uint32_t *) calloc(item_count, sizeof(uint32_t));
 
-  uint32_t** pop1 = (uint32_t **) malloc(pop_size * sizeof(uint32_t *));
-  for (uint32_t i = 0; i < pop_size; i++) {
-    pop1[i] = (uint32_t *) calloc(item_count / 32, sizeof(uint32_t));
-  }
-
-  uint32_t** pop = pop0;
-  uint32_t** new_pop = pop1;
+  uint32_t* pop = pop0;
+  uint32_t* new_pop = pop1;
   uint8_t cycle = 0;
   gen_population_nums(pop, pop_size, item_count);
   //gen_population_bits(pop, pop_size, item_count, item_count / 4);
-  uint64_t *ranking = calloc(pop_size, sizeof(uint64_t));
-  uint64_t rank_sum, max = 0, score;
-  uint64_t parent1_score, parent2_score;
+  uint32_t *ranking = calloc(pop_size, sizeof(uint32_t));
+  uint32_t rank_sum = 0, max = 0, score = 0;
+  uint32_t parent1_score, parent2_score;
   int64_t parent1_idx, parent2_idx;
 
   for (uint32_t c = 0; c < cycles; c++) {
@@ -101,16 +144,21 @@ void cycle(knapsack_node_t *knapsack, uint32_t pop_size,
       rank_sum = 0;
 
       for (uint32_t i = 0; i < pop_size; i++) {
-        score = test_fitness(pop[i], item_count, knapsack, max_weight);
+        score = test_fitness(&pop[i * item_count], item_count, knapsack, max_weight);
+
         ranking[i] = score;
         rank_sum += score;
       }
 
-      while (parent1_score == 0) {
-      	parent1_score = rand() % rank_sum;
+      if (rank_sum == 0) {
+          return;
       }
-      while (parent2_score == 0) {
-      	parent2_score = rand() % rank_sum;
+
+      while (parent1_score == 0) {
+        parent1_score = rand() % rank_sum;
+      }
+      while (parent2_score == 0 || parent2_score == parent1_score) {
+        parent2_score = rand() % rank_sum;
       }
       rank_sum = 0;
 
@@ -129,8 +177,30 @@ void cycle(knapsack_node_t *knapsack, uint32_t pop_size,
         }
       }
 
-      breed(new_pop[x], new_pop[x + 1], pop[parent1_idx], pop[parent2_idx], pop_size / 64);
+      breed(&new_pop[x * item_count], &new_pop[(x + 1) * item_count],
+            &pop[parent1_idx * item_count], &pop[parent2_idx * item_count],
+            pop_size); // (2 * 8 * sizeof(uint32_t))
+
+        mutate(&new_pop[x * item_count], item_count);
+        mutate(&new_pop[(x + 1) * item_count], item_count);
+
     }
+
+    // The population will not necessarily converge to the optimal solution,
+    // so record the best seen cell out of all previously seen cells at each
+    // generation.
+    for (uint16_t i = 0; i < pop_size; i++) {
+        uint32_t cell_weight = get_weight(&new_pop[i * item_count], item_count, knapsack);
+        if (cell_weight < max_weight) {
+            score = get_value(&new_pop[i * item_count], item_count, knapsack);
+
+            if (score > max) {
+                max = score;
+                memcpy(best_cell, &new_pop[i * item_count], item_count * sizeof(uint32_t));
+            }
+        }
+    }
+
     if (cycle == 0) {
       pop = pop1;
       new_pop = pop0;
@@ -142,38 +212,29 @@ void cycle(knapsack_node_t *knapsack, uint32_t pop_size,
     }
   }
 
-  for (uint16_t i = 0; i < pop_size; i++) {
-    score = test_fitness(pop[i], item_count, knapsack, max_weight);
-    max = score > max ? score : max;
+  if (max == 0) {
+      printf("No solutions found\n");
+  } else {
+      print_cell_nodes(best_cell, item_count, knapsack);
   }
 
-  printf("max weight: %llu\n", max);
-
-  for (uint32_t i = 0; i < pop_size; i++) {
-    free(pop[i]);
-  }
-  free(pop);
-
-  for (uint32_t i = 0; i < pop_size; i++) {
-    free(new_pop[i]);
-  }
-  free(new_pop);
-
+  free(pop0);
+  free(pop1);
   free(ranking);
+  free(best_cell);
 }
 
 int main(int argc, char **argv)
 {
-  uint32_t pop_size = 100;
-  uint32_t item_count = 16;
-  //uint32_t item_count = 1024; // Must be a power of two.
+  uint32_t pop_size = 16;
+  uint32_t item_count = 16; // Must be a power of two.
   uint32_t max_value = 100;
   uint32_t max_weight = 100;
   uint32_t cycles = 100;
 
   knapsack_node_t *nodes = (knapsack_node_t*) calloc(item_count, sizeof(knapsack_node_t));
   gen_nodes(nodes, item_count, max_value, max_weight);
-  print_nodes(nodes, item_count);
+  //print_nodes(nodes, item_count);
 
   cycle(nodes, pop_size, item_count, max_weight, cycles);
 
