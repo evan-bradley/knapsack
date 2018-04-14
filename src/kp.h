6a171e79995a29b20fcb0ff93b8f5898b0c5c128
Evan Bradley <evanbradley@airmail.cc>
2018-04-14 16:01:03 -0400

    Added functions to record knapsack data.

--- src/kp.h
+++ src/kp.h
@@ -8,6 +8,25 @@ typedef struct knapsack_node_s {
   uint32_t weight;
 } knapsack_node_t;
 
+typedef struct parameters_s {
+  uint32_t pop_size;
+  uint32_t item_count; // Must be a power of two.
+  uint32_t max_value;
+  uint32_t max_weight;
+  uint32_t cycles;
+  uint32_t mutation_prob;
+  uint32_t migration_size;
+  uint32_t migration_freq;
+} parameters_t;
+
+typedef struct file_header_s {
+  uint32_t item_count;
+  uint32_t max_value;
+  uint32_t max_weight;
+  uint32_t best_value;
+  uint32_t best_weight;
+} file_header_t;
+
 void gen_nodes(knapsack_node_t *knapsack, uint32_t nodes, uint32_t max_value, uint32_t max_weight)
 {
   srand(100);
@@ -25,14 +44,24 @@ void print_nodes(knapsack_node_t *knapsack, uint32_t nodes)
   }
 }
 
-typedef struct parameters_s {
-  uint32_t pop_size;
-  uint32_t item_count; // Must be a power of two.
-  uint32_t max_value;
-  uint32_t max_weight;
-  uint32_t cycles;
-  uint32_t mutation_prob;
-  uint32_t migration_size;
-  uint32_t migration_freq;
-} parameters_t;
+void write_data(file_header_t *header, knapsack_node_t *nodes, uint32_t *best, char *filename)
+{
+    FILE *data = fopen(filename, "wb");
 
+    fwrite(header, sizeof(file_header_t), 1, data);
+    fwrite(best, sizeof(uint32_t), header->item_count, data);
+    fwrite(nodes, sizeof(knapsack_node_t), header->item_count, data);
+
+    fclose(data);
+}
+
+void read_data(file_header_t *header, knapsack_node_t *nodes, uint32_t *best, char *filename)
+{
+    FILE *data = fopen(filename, "rb");
+
+    fread(header, sizeof(file_header_t), 1, data);
+    fread(best, sizeof(uint32_t), header->item_count, data);
+    fread(nodes, sizeof(knapsack_node_t), header->item_count, data);
+
+    fclose(data);
+}
--- src/main.c
+++ src/main.c
@@ -1,11 +1,6 @@
 #include "kp.h"
 #include <time.h>
 
-// Keep[N][W]
-uint32_t **keep;
-// V[n + 1][W + 1]
-uint32_t **V;
-
 /**
  *
  * @param node The knapsack potential entries
@@ -13,18 +8,21 @@ uint32_t **V;
  * @param W The total size possible
  * @return The optimal solution
  */
-int Knapsack(knapsack_node_t *nodes, uint32_t n, uint32_t W)
+knapsack_node_t Knapsack(knapsack_node_t *nodes, uint32_t **V,
+                         uint32_t **keep, file_header_t opt)
 {
 
-    for (uint32_t i = 0; i < W; i++)
+    knapsack_node_t best = {0, 0};
+
+    for (uint32_t i = 0; i < opt.max_weight; i++)
     {
         V[0][i] = 0;
     }
 
-    for (uint32_t i = 1; i <= n; i++)
+    for (uint32_t i = 1; i <= opt.item_count; i++)
     {
         knapsack_node_t node = nodes[i];
-        for (uint32_t j = 0; j <= W; j++)
+        for (uint32_t j = 0; j <= opt.max_weight; j++)
         {
             if ((node.weight <= j) && (node.value + V[i - 1][j - node.weight] > V[i - 1][j]))
             {
@@ -38,18 +36,19 @@ int Knapsack(knapsack_node_t *nodes, uint32_t n, uint32_t W)
             }
         }
     }
-    uint32_t K = W;
-    for (int i = n; i > 0; i--)
+    uint32_t K = opt.max_weight;
+    for (int i = opt.item_count; i > 0; i--)
     {
         knapsack_node_t node = nodes[i];
         if (keep[i][K] == 1)
         {
-            // output i
-          printf("Good i: %d; %u, %u\n", i, nodes[i].value, nodes[i].weight);
+            best.weight += nodes[i].weight;
             K = K - node.weight;
         }
     }
-    return V[n][W];
+
+    best.value = V[opt.item_count][opt.max_weight];
+    return best;
 }
 
 uint32_t* allocate_mem(uint32_t*** arr, uint32_t n, uint32_t m)
@@ -61,29 +60,48 @@ uint32_t* allocate_mem(uint32_t*** arr, uint32_t n, uint32_t m)
     return arr_data; //free pouint32_t
 }
 
-void deallocate_mem(uint32_t*** arr, uint32_t* arr_data){
+void deallocate_mem(uint32_t*** arr, uint32_t* arr_data)
+{
     free(arr_data);
     free(*arr);
 }
 
 int main()
 {
-    uint32_t node_count = 16;
-    uint32_t max_value = 100;
-    uint32_t max_weight = 100;
+    file_header_t options = {
+      16,  // item_count
+      100, // max_value
+      100, // max_weight
+      0, // best_value
+      0, // best_weight
+    };
+
+    // Keep[N][W]
+    uint32_t **keep;
+    // V[n + 1][W + 1]
+    uint32_t **V;
 
     // Generate keep and V
-    uint32_t *keep_point = allocate_mem(&keep, node_count + 1, max_weight + 1);
-    uint32_t *V_point = allocate_mem(&V, node_count + 1, max_weight + 1);
+    uint32_t *keep_point = allocate_mem(&keep, options.item_count + 1, options.max_weight + 1);
+    uint32_t *V_point = allocate_mem(&V, options.item_count + 1, options.max_weight + 1);
+    uint32_t *best = (uint32_t *) calloc(options.item_count, sizeof(uint32_t));
 
     // Generate the nodes
-    knapsack_node_t *nodes = (knapsack_node_t*) calloc(node_count, sizeof(knapsack_node_t));
-    gen_nodes(nodes, node_count, max_value, max_weight);
-    print_nodes(nodes, node_count);
+    knapsack_node_t *nodes = (knapsack_node_t*) calloc(options.item_count, sizeof(knapsack_node_t));
+    gen_nodes(nodes, options.item_count, options.max_value, options.max_weight);
+    //print_nodes(nodes, options.item_count);
 
     // Evaluate
-    uint32_t maxVal = Knapsack(nodes, node_count, max_weight);
-    printf("Max values: %d\n", maxVal);
+    knapsack_node_t maxVal = Knapsack(nodes, V, keep, options);
+    printf("Maximum: value %u, weight %u\n", maxVal.value, maxVal.weight);
+    options.best_value = maxVal.value;
+    options.best_weight = maxVal.weight;
+
+    for (uint32_t i = 0; i < options.item_count; i++) {
+        best[i] = keep[i][options.max_weight];
+    }
+
+    write_data(&options, nodes, best, "test.kp");
 
     free(nodes);
     deallocate_mem(&keep, keep_point);
